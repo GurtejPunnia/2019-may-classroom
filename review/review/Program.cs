c8853ece1a53ca6cb975b6395bb075412e93fa81
GurtejPunnia <48194711+GurtejPunnia@users.noreply.github.com>
2019-05-06 18:46:51 -0600

    review2

--- review/review/Program.cs
+++ review/review/Program.cs
@@ -10,8 +10,34 @@ namespace review
     {
         static void Main(string[] args)
         {
+            //Console is a reference to a system static class
+            //static classes are used typing the class name
+            //static classes do not store variable data
+            //static classes are used by everyone
+            //methods of a static class will be placed after 
+            // the object dot operatator
             Console.WriteLine("Hello");
+            
+
+            //create an instance of a non static class
+            //if you create a declarative using just the class name
+            //then this is only a pointer that can hold an instance of the class
+            //specified;
+            //current value of aVariable is null
+
+            review2 variable;
+
+            //if you use a new command with the class
+            // the new command will phtically create an instance of class
+
+            review2 newvariable = new review2();
+
+            if(newvariable == null)
+            {
+                Console.WriteLine("newvariable is empty");
+            }
             Console.ReadLine();
         }
+
     }
 }
---
+++ review/review/review2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace review
+{
+    //the default permission is private
+    // if an outside user of this class wanted
+    // access to the class contents; then
+    // the access permission needs to be public.
+    //if this class was static the class would not 
+    // hold data.
+    //For a class to hold data do not make  it static 
+    // unless you want everyone to have access to that data (read and change).
+    class review2
+    {
+        public int Height, Width;
+      
+    }
+}
